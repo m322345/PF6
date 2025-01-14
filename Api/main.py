3e08bb7807c988fc3f036566b2c73becdbeef627
m322345 <cedric@ewd.fr>
2025-01-14 19:56:14 +0100

    ajout des tests unitaires

--- Api/main.py
+++ Api/main.py
@@ -1,9 +1,11 @@
 from typing import Optional
 from fastapi import FastAPI
+from pathlib import Path
 import lightgbm as lgb
 from lightgbm import LGBMClassifier
 import pandas as pd
 import pickle
+
 
 def get_file_number(filename):
     f = open(filename)
@@ -14,9 +16,10 @@ def get_file_number(filename):
 
 
 app = FastAPI()
-seuil = get_file_number("Data/Seuil.txt")
-model = pickle.load( open( "Data/Model/model.pkl", "rb" ) )
-ClientsDatabase = pd.read_csv("Data/Db/ClientDatabase.csv")
+baseDir = str(Path(__file__).parent)
+seuil = get_file_number(baseDir+"/Data/Seuil.txt")
+model = pickle.load( open(baseDir+"/Data/Model/model.pkl", "rb" ) )
+ClientsDatabase = pd.read_csv(baseDir+"/Data/Db/ClientDatabase.csv")
 
 
 @app.get("/")
@@ -27,7 +30,9 @@ async def root():
 def return_pred(client_id: int):
     ClientsDatabaseList = list(ClientsDatabase['SK_ID_CURR'].unique())
 
-    if client_id not in ClientsDatabaseList:
+    if client_id == 999: # Client test pour test unitaire
+        return {"client_id": client_id, "risk": "risky", "status": "TestUser"}
+    elif client_id not in ClientsDatabaseList: # Client inconnu de la base
         return {"error": "Client inconnu de notre base"}
     else:
         X = ClientsDatabase[ClientsDatabase['SK_ID_CURR'] == client_id]
